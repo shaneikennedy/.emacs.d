6e476361747ee3fe0f3c043729bddee8999a3038
Gov <info@govindmohan.com>
2021-03-02 23:25:42 -0500

    clang-format

--- modules/my-cpp.el
+++ modules/my-cpp.el
@@ -10,9 +10,33 @@
 (use-package ccls
   :hook ((c-mode c++-mode objc-mode cuda-mode) .
          (lambda () (require 'ccls) (lsp))))
-(setq lsp-diagnostic-package nil)
-(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-(setq ccls-executable "/path/to/ccls/Release/ccls")
+
+;; Run clang-format on save
+(defun clang-format-save-hook-for-this-buffer ()
+  "Create a buffer local save hook."
+  (add-hook 'before-save-hook
+    (lambda ()
+      (progn
+        (when (locate-dominating-file "." ".clang-format")
+          (clang-format-buffer))
+        ;; Continue to save.
+        nil))
+    nil
+    ;; Buffer local hook.
+    t))
+
+;; Run this for each mode you want to use the hook.
+(add-hook 'c-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
+(add-hook 'c++-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
+
+;; This has to be visible in PATH
+(setq ccls-executable "ccls")
+
+(defun lsp-file-watch-ignored ()
+  lsp-file-watch-ignored)
+
+;; Allow lsp-file-watch-ignored as a file or directory-local variable
+(put 'lsp-file-watch-ignored 'safe-local-variable 'lsp--string-listp)
 
 (provide 'my-cpp)
 ;;; my-cpp.el ends here

a5dad67f7a4758f096197136400dece3558da42f
Shane Kennedy <shane.kennedy19@gmail.com>
2021-07-16 15:17:20 +0200

    Use bazel-mode package

--- modules/my-configs.el
+++ modules/my-configs.el
@@ -6,7 +6,9 @@
 (use-package yaml-mode)
 (use-package jenkinsfile-mode)
 (use-package toml-mode)
-(use-package bazel-mode)
+(use-package bazel)
+(add-to-list 'auto-mode-alist '("BUILD\\'" . bazel-mode))
+(add-to-list 'auto-mode-alist '("WORKSPACE\\'" . bazel-mode))
 (use-package protobuf-mode)
 (use-package terraform-mode)
 

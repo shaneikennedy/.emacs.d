f05a95f805af368f428f0c34c0b6fe9a3c08421b
Shane Kennedy <shane.kennedy19@gmail.com>
2022-10-10 16:18:24 +0200

    Steal casing functions, see changes for source

--- modules/my-functions.el
+++ modules/my-functions.el
@@ -135,5 +135,27 @@
   (interactive)
   (transient-setup 'git-sync))
 
+(defun camelcase-region (start end)
+  "Change region (START END) from snake_case to camelCase."
+  (interactive "r")
+  (save-restriction (narrow-to-region start end)
+                    (goto-char (point-min))
+                    (while (re-search-forward "_\\(.\\)" nil t)
+                      (replace-match (upcase (match-string 1))))))
+
+;; ----------------------------------------------------------------------
+;; cadged largely from http://xahlee.org/emacs/elisp_idioms.html:
+;;
+(defun camelcase-word-or-region ()
+  "Change word or region from snake_case to camelCase."
+  (interactive)
+  (let (pos1 pos2 bds)
+    (if (and transient-mark-mode mark-active)
+        (setq pos1 (region-beginning) pos2 (region-end))
+      (progn
+        (setq bds (bounds-of-thing-at-point 'symbol))
+        (setq pos1 (car bds) pos2 (cdr bds))))
+    (camelcase-region pos1 pos2)))
+
 (provide 'my-functions)
 ;;; my-functions.el ends here

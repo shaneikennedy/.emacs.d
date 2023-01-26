9a18144103df11c74d6473ecc8708ecc9fa1067d
Shane Kennedy <shane.kennedy19@gmail.com>
2023-01-26 09:08:18 +0100

    Get rid of dumb jump

--- init.el
+++ init.el
@@ -89,21 +89,6 @@
   (when (executable-find "pandoc")
     (setq markdown-command "pandoc -f markdown -t html")))
 
-(use-package dumb-jump)
-(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
-(setq xref-show-definitions-function #'xref-show-definitions-completing-read)
-(use-package ivy-xref
-  :ensure t
-  :init
-  ;; xref initialization is different in Emacs 27 - there are two different
-  ;; variables which can be set rather than just one
-  (when (>= emacs-major-version 27)
-    (setq xref-show-definitions-function #'ivy-xref-show-defs))
-  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
-  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
-  ;; as well
-  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
-
 (use-package zig-mode
   :bind ("M-q" . zig-format-buffer))
 (setq zig-format-on-save nil)

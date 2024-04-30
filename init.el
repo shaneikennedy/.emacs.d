8a94feba02d7e9f3f5fe5711b2835c4fcade6ba9
Shane Kennedy <shane.kennedy@klarna.com>
2024-04-30 15:14:05 +0200

    Tree sitter

--- init.el
+++ init.el
@@ -61,6 +61,20 @@
   :config
   (diminish 'eldoc-mode))
 
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
+
 ;; Custom modules
 (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
 (require 'my-gui)

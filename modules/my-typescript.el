416fc40af7548e96b9034d00d1126ec28476f849
shane kennedy <shane.kennedy@klarna.com>
2025-01-14 09:51:33 +0100

    Fix typescript module

--- modules/my-typescript.el
+++ modules/my-typescript.el
@@ -5,10 +5,13 @@
 
 ;;; Code:
 (use-package typescript-mode)
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'"    . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'"    . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"    . tsx-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'"    . tsx-ts-mode))
 
-(add-hook 'typescript-mode-hook #'lsp)
-(add-hook 'typescript-ts-mode-hook #'lsp)
+(add-hook 'tsx-ts-mode-hook #'lsp-deferred)
+(add-hook 'typescript-ts-mode-hook #'lsp-deferred)
 
 (use-package npm)
 
